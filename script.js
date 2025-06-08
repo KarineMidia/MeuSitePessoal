6a610f7cb1e8c75bfcfcdc069d8c7b77490fc27c
KarineM <criativamidiaoficial@gmail.com>
2025-06-08 13:07:33 -0400

    ultima aula modulo 2

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-alert("Bem-vindo ao meu site 😍")
+document.addEventListener("DOMContentLoaded", () => {
+    alert("Bem-vindo ao meu site 😍")
+})
 
 const titulo =document.querySelector("h1")
 
-titulo.style.textAlign = "right"
 titulo.style.color= "gray"
 titulo.style.fontSize = "20px"
 
 const cards = [
     {titulo: "Criativa Mídia", descricao: "Serviços de Mídias Digitais"},
     {titulo: "Psicologia", descricao: "Acolhimento Psicoligo"},
-    {titulo: "Autoestima", descricao: "Serviços de estetica"}
+    {titulo: "Beleza", descricao: "Serviços de estetica"}
 ]
 
 const quadros = document.querySelector(".cards")
@@ -27,3 +28,29 @@ for(let i = 0; i < cards.length; i++){
     quadros.append(card)
 
 }
+const frases = [
+    "A vida é feita de escolhas.",
+    "A persistência é o caminho do êxito.",
+    "A gratidão transforma o que temos em suficiente.",
+    "O sucesso é a soma de pequenos esforços repetidos dia após dia.",
+    "A felicidade não é algo pronto. Ela vem de suas próprias ações.",
+    "Você é capaz de realizar coisas incríveis.",
+    "Acredite em si mesmo e tudo será possível.",
+    "A vida é uma jornada, aproveite cada passo.",
+    "A mudança é o primeiro passo para o crescimento.",
+    "A simplicidade é o último grau de sofisticação.",
+    "A beleza está nos olhos de quem vê.",
+    "A vida é uma aventura, aproveite cada momento.",
+    "A paciência é amarga, mas seus frutos são doces.",
+    "A sabedoria começa na reflexão.",
+    "A vida é curta, faça valer a pena.",
+    "A alegria é contagiante, espalhe-a por onde for.",
+    "Você é mais forte do que imagina.",
+]; 
+
+const foto = document.querySelector(".minhafoto")
+function mostrarFrase() {
+    const indice = Math.floor(Math.random() * frases.length);
+    alert(frases[indice]);
+}
+foto.addEventListener("click", mostrarFrase);
